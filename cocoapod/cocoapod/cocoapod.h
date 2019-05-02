2844d013171dbf7cad2975e518a6ae86d536d0b5
Antonio Brito <antoniobrito@Antonio-Brito.local>
2019-05-02 16:12:11 -0500

    Prueba de Cocoa Pods Frameworks

---
+++ cocoapod/cocoapod/cocoapod.h
@@ -0,0 +1,19 @@
+//
+//  cocoapod.h
+//  cocoapod
+//
+//  Created by Antonio Brito on 5/2/19.
+//  Copyright © 2019 Antonio Brito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cocoapod.
+FOUNDATION_EXPORT double cocoapodVersionNumber;
+
+//! Project version string for cocoapod.
+FOUNDATION_EXPORT const unsigned char cocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cocoapod/PublicHeader.h>
+
+
---
+++ cocoapod/cocoapod/service.swift
@@ -0,0 +1,16 @@
+//
+//  service.swift
+//  cocoapod
+//
+//  Created by Antonio Brito on 5/2/19.
+//  Copyright © 2019 Antonio Brito. All rights reserved.
+//
+
+import Foundation
+
+public class service {
+    private init () {}
+    public static func doSomething() -> String {
+        return "Pruebas para cocoa pods"
+    }
+}
